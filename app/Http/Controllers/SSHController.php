70be505d1acfdd00914d6e06b3dc05657bbf7572
Anischenko Viktor <design.vq@gmail.com>
2023-08-28 20:42:58 +0500

    ssh merge conflict

--- app/Http/Controllers/SSHController.php
+++ app/Http/Controllers/SSHController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SSHController extends Controller
 {
-    public function ssgFromGitLaravel2() {
+    public function ssgFromGitLaravel() {
         return '';
     }
 }
